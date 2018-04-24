2267e722fe220472bf0d6106d11436d7ed613363
DevExpressExampleBot <fakeEmail>
2018-04-24 20:43:45 +0300

    Source auto update [skip ci]

--- VB/CustomItem/Form1.vb
+++ VB/CustomItem/Form1.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -14,31 +13,32 @@ Imports DevExpress.XtraPrinting.Preview.Native
 Imports DevExpress.XtraPrinting
 
 Namespace CustomItem
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits Form
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			Dim tool As New ReportPrintTool(New XtraReport1())
-			AddHandler tool.PreviewForm.Shown, AddressOf PreviewForm_Shown
-			tool.ShowPreviewDialog()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub PreviewForm_Shown(ByVal sender As Object, ByVal e As EventArgs)
-			Dim form As PrintPreviewFormEx = CType(sender, PrintPreviewFormEx)
-			Dim item As PrintPreviewBarItem = CType(form.PrintBarManager.GetBarItemByCommand(PrintingSystemCommand.ExportFile), PrintPreviewBarItem)
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            Dim tool As New ReportPrintTool(New XtraReport1())
+            AddHandler tool.PreviewForm.Shown, AddressOf PreviewForm_Shown
+            tool.ShowPreviewDialog()
+        End Sub
 
-			Dim control As PopupMenu = CType((CType(item, DevExpress.XtraBars.BarButtonItem)).DropDownControl, PopupMenu)
-			Dim barItem As New BarButtonItem()
-			AddHandler barItem.ItemClick, AddressOf barItem_ItemClick
-			barItem.Caption = "Custom Item"
-			control.AddItem(barItem)
-		End Sub
+        Private Sub PreviewForm_Shown(ByVal sender As Object, ByVal e As EventArgs)
+            Dim form As PrintPreviewFormEx = DirectCast(sender, PrintPreviewFormEx)
+            Dim item As PrintPreviewBarItem = CType(form.PrintBarManager.GetBarItemByCommand(PrintingSystemCommand.ExportFile), PrintPreviewBarItem)
 
-		Private Sub barItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-			MessageBox.Show("Item is clicked!")
-		End Sub
-	End Class
+            Dim control As PopupMenu = CType(CType(item, DevExpress.XtraBars.BarButtonItem).DropDownControl, PopupMenu)
+            Dim barItem As New BarButtonItem()
+            AddHandler barItem.ItemClick, AddressOf barItem_ItemClick
+            barItem.Caption = "Custom Item"
+            control.AddItem(barItem)
+        End Sub
+
+        Private Sub barItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            MessageBox.Show("Item is clicked!")
+        End Sub
+    End Class
 End Namespace
